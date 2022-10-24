1c8e4e35cb070b076ec04cfe3e2ad7d6425ccc36
brwuna <brunan.santos97@gmail.com>
2022-10-24 17:28:24 -0300

    change route

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,7 +5,7 @@ import { stripe } from "../../lib/stripe";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { products } = req.body as { products: IProduct[] };
 
-    if (req.method !== 'POST') {
+    if (req.method !== 'GET') {
         return res.status(405).json({error: 'Method not allowed.'});
     }
 
