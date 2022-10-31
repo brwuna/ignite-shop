e9e616b32be847f59669127216434c19ea45d7b1
brwuna <brunan.santos97@gmail.com>
2022-10-31 17:52:49 -0300

    successUrl

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({error: 'Products not found.'})
     }
 
-    const successUrl = `${process.env.NEXT_URL_SUCCESS}`;
+    const successUrl = process.env.NEXT_URL_SUCCESS;
     const cancelUrl = `${process.env.NEXT_URL}/`;
 
     const checkoutSession = await stripe.checkout.sessions.create({
